a69a8ebe7ee1ea82569e5f494a5d2d792e1a6448
Burcu Çanga Zeytin <cangaburcu@gmail.com>
2021-12-09 22:47:56 +0300

    getClickedPosition -1 -1 donuyor ama calisiyor

--- connect4BitirmeProjesi.py
+++ connect4BitirmeProjesi.py
@@ -1,106 +1,41 @@
+from ctypes import sizeof
 from tkinter import *
+class Menu:
+    def addMenu(self):
+        self.menubar = Menu(theWindow)
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="New", command=self.donothing)
+        self.filemenu.add_command(label="Open", command=self.donothing)
+        self.filemenu.add_command(label="Save", command=self.donothing)
+        self.filemenu.add_command(label="Save as...", command=self.donothing)
+        self.filemenu.add_command(label="Close", command=self.donothing)
 
+        self.filemenu.add_separator()
 
-# class Menu:
-#     def addMenu(self):
-#         self.menubar = Menu(window)
-#         self.filemenu = Menu(self.menubar, tearoff=0)
-#         self.filemenu.add_command(label="New", command=self.donothing)
-#         self.filemenu.add_command(label="Open", command=self.donothing)
-#         self.filemenu.add_command(label="Save", command=self.donothing)
-#         self.filemenu.add_command(label="Save as...", command=self.donothing)
-#         self.filemenu.add_command(label="Close", command=self.donothing)
+        self.filemenu.add_command(label="Exit", command=theWindow.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.editmenu = Menu(self.menubar, tearoff=0)
+        self.editmenu.add_command(label="Undo", command=self.donothing)
 
-#         self.filemenu.add_separator()
+        self.editmenu.add_separator()
 
-#         self.filemenu.add_command(label="Exit", command=window.quit)
-#         self.menubar.add_cascade(label="File", menu=self.filemenu)
-#         self.editmenu = Menu(self.menubar, tearoff=0)
-#         self.editmenu.add_command(label="Undo", command=self.donothing)
+        self.editmenu.add_command(label="Cut", command=self.donothing)
+        self.editmenu.add_command(label="Copy", command=self.donothing)
+        self.editmenu.add_command(label="Paste", command=self.donothing)
+        self.editmenu.add_command(label="Delete", command=self.donothing)
+        self.editmenu.add_command(label="Select All", command=self.donothing)
 
-#         self.editmenu.add_separator()
+        self.menubar.add_cascade(label="Edit", menu=self.editmenu)
+        self.helpmenu = Menu(self.menubar, tearoff=0)
+        self.helpmenu.add_command(label="Help Index", command=self.donothing)
+        self.helpmenu.add_command(label="About...", command=self.donothing)
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu)
+        self.window.config(menu=self.menubar)
 
-#         self.editmenu.add_command(label="Cut", command=self.donothing)
-#         self.editmenu.add_command(label="Copy", command=self.donothing)
-#         self.editmenu.add_command(label="Paste", command=self.donothing)
-#         self.editmenu.add_command(label="Delete", command=self.donothing)
-#         self.editmenu.add_command(label="Select All", command=self.donothing)
-
-#         self.menubar.add_cascade(label="Edit", menu=self.editmenu)
-#         self.helpmenu = Menu(self.menubar, tearoff=0)
-#         self.helpmenu.add_command(label="Help Index", command=self.donothing)
-#         self.helpmenu.add_command(label="About...", command=self.donothing)
-#         self.menubar.add_cascade(label="Help", menu=self.helpmenu)
-#         self.window.config(menu=self.menubar)
-
-#     def donothing(self):
-#         filewin = Toplevel(window)
-#         button = Button(filewin, text="Do nothing button")
-#         button.pack()
-
-
-
-class Move:
-    theButton = Button(width=5, height=5)
-    theTurn = 0
-
-    def __init__(self, myButton, myTurn): 
-        self.theButton = myButton
-        self.theTurn = myTurn
-
-
-
-class App:
-    container = {}
-
-    def insertDisc(self, move):
-        
-        if move.theTurn == yellowCoin:
-            move.theButton['command'] = lambda theButton=move.theButton: insertYellowDisc(move.theButton)
-        elif move.theTurn == redCoin:
-            move.theButton['command'] = lambda theButton=move.theButton: insertRedDisc(move.theButton)
-        else:
-            print("Error")
-        move.theButton.pack()
-
-    def getClickedPosition(event):
-        # Here retrieving the size of the parent
-        # widget relative to master widget
-        x = event.x_root - window.frame.winfo_rootx()
-        y = event.y_root - window.frame.winfo_rooty()
-        
-        # Here grid_location() method is used to
-        # retrieve the relative position on the
-        # parent widget
-        z = window.frame.grid_location(x, y)
-    
-        # printing position
-        print(z)
-        return z
-
-
-    def play(self):   
-        turn = yellowCoin
-        for i in range(42):
-            position = ""
-            window.bind("<Button-1>", lambda position : self.getClickedPosition)
-            print(position)
-            # move = Move(self.container[0,0], turn)
-            # self.insertDisc(move)
-    
-
-            turn = switchTurn(turn)  
-
-    def __init__(self) -> None:
-
-        for i in range(6):
-            for j in range(7):
-                frame = Frame(master=window, relief=FLAT, borderwidth=5, bg="blue")
-                frame.grid(row=i, column=j, padx=1, pady=1)
-                myButton = Button(master=frame, width=5, height=5, text=str(i)+ ", " + str(j))
-                myButton.pack()
-                self.container[i,j] = myButton
-
+    def donothing(self):
+        filewin = Toplevel(theWindow)
+        button = Button(filewin, text="Do nothing button")
+        button.pack()
 
 
 def insertYellowDisc(buttonParam):
@@ -118,23 +53,78 @@ def switchTurn(myTurn):
         switchedTurn = yellowCoin
     return switchedTurn
 
+
+def getClickedPosition(event, frame):
+    print(frame["bg"])
+    # Here retrieving the size of the parent
+    # widget relative to master widget
+    x = abs(event.x_root - frame.winfo_rootx())
+    y = abs(event.y_root - frame.winfo_rooty())
+    
+    # Here grid_location() method is used to
+    # retrieve the relative position on the
+    # parent widget
+    z = frame.grid_location(x, y)
+
+    # printing position
+    print(z)
+    return z
+
+class App:
+    table = {}
+
+    def __init__(self, myWindow, myTurn) -> None:
+        
+        for i in range(6):
+            for j in range(7):
+                key = (i, j)
+                theFrame = Frame(master=myWindow, relief=FLAT, borderwidth=5, bg="blue")
+                theFrame.grid(row=i, column=j, padx=1, pady=1)
+                theWindow.update()
+                myButton = Button(master=theFrame, width=5, height=5, text=str(i) + "," + str(j)) 
+
+                myButton.bind("<Button-1>", lambda event:getClickedPosition(event, theFrame))
+                # print(str(theFrame.winfo_rootx()) + "," + str(theFrame.winfo_rootx()))
+                myButton.pack()
+                self.table[key]= myButton
+                
+
+
+     
+    def playGame(self, whoseTurn, myButton):
+        
+        if whoseTurn == yellowCoin:
+            myButton['command'] = lambda theButton=myButton: insertYellowDisc(theButton)
+        elif whoseTurn == redCoin:
+            myButton['command'] = lambda theButton=myButton: insertRedDisc(theButton)
+        else:
+            print("Error")
+        whoseTurn = switchTurn(whoseTurn)
+
+
 if __name__ == "__main__":
-    window = Tk()
+    theWindow = Tk()
 
     # Setting the title of the window
-    window.title("Connect 4")
+    theWindow.title("Connect 4")
 
-    window['bg'] = "blue"
+    # Setting the geometry i.e Dimensions
+    theWindow.geometry('1000x1000')
+
+    theWindow['bg'] = "blue"
 
     # Calling our App
-    # menu = Menu()
+    menu = Menu()
 
     yellowCoin = 0
     redCoin = 1
     turn = yellowCoin
 
     # Calling our App
-    app = App()
-    app.play()
+    app = App(theWindow, turn)
+    for i in range(42):
+        key = (1, 2)
+        app.playGame(turn, app.table[key])
+
     # to run infinitely
-    window.mainloop()
+    theWindow.mainloop()
