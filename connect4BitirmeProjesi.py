954fdd8e3b8e7b8c3d9e46a03b1f96cb356b4e57
Burcu Çanga Zeytin <cangaburcu@gmail.com>
2021-12-10 23:29:58 +0300

    GUI bu is olmus

--- connect4BitirmeProjesi.py
+++ connect4BitirmeProjesi.py
@@ -42,82 +42,74 @@ table = {}
 movesDict = {}
 
 
-def switchTurn(myTurn, i, j):
-    if myTurn == yellowCoin:
-        switchedTurn = redCoin
-    else:
-        (table[i,j])["bg"] = "red"
-        switchedTurn = yellowCoin
-    return switchedTurn
 
-def getClickedPosition(event, frame):
-    print(frame["bg"])
-    # Here retrieving the size of the parent
-    # widget relative to master widget
-    x = abs(event.x_root - frame.winfo_rootx())
-    y = abs(event.y_root - frame.winfo_rooty())
+
+# def getClickedPosition(event, frame):
+#     # Here retrieving the size of the parent
+#     # widget relative to master widget
+#     x = abs(event.x_root - frame.winfo_rootx())
+#     y = abs(event.y_root - frame.winfo_rooty())
     
-    # Here grid_location() method is used to
-    # retrieve the relative position on the
-    # parent widget
-    z = frame.grid_location(x, y)
+#     # Here grid_location() method is used to
+#     # retrieve the relative position on the
+#     # parent widget
+#     z = frame.grid_location(x, y)
 
-    # printing position
-    print(z)
-    return z
+#     # printing position
+#     print(z)
+#     return z
 
-def whichButton(i, j, buttonParam):
-    movesDict[0,0] = buttonParam
-    if(turn == yellowCoin):
-        (table[i,j])["bg"] = "yellow"
-    else:
-        (table[i,j])["bg"] = "red"
-    switchTurn(turn, i, j)
+
+
 
 class App:
+    turn = 0
 
-    def __init__(self, myWindow, myTurn) -> None:
+    # def switchTurn():
+    #     if turn == 0:
+    #         switchedTurn = 1
+    #     else:
+    #         switchedTurn = 0
+    #     return switchedTurn
+
+    def whichButton(self, i, j, buttonParam):
+        movesDict[0,0] = buttonParam
+        if(self.turn == 0):
+            (table[i,j])["bg"] = "yellow"
+            self.turn = 1
+        else:
+            (table[i,j])["bg"] = "red"
+            self.turn = 0
+
+
+    def __init__(self, myWindow) -> None:
+
         for i in range(0,6):
             for j in range(0,6):
                 key = (i, j)
                 theFrame = Frame(master=myWindow, relief=FLAT, borderwidth=5, bg="blue")
                 theFrame.grid(row=i, column=j, padx=1, pady=1)
 
-                if(i == 0):
-                    myButton = Button(master=theFrame, width=1, height=1, text=str(j)) 
-                    myButton["command"] = lambda k=i, m=j , buttonParam = myButton: whichButton(k, m, buttonParam)
-                    # myButton.bind("<Button-1>", playGame)
-                    myButton.pack()
-                else:
-                    myButton = Button(master=theFrame, width=5, height=5, text=str(i-1) + "," + str(j), padx=5, pady=5)
-                    myButton.pack()
-
+                myButton = Button(master=theFrame, width=5, height=5, text=str(i) + "," + str(j), padx=5, pady=5)
+                myButton["command"] = lambda k=i, m=j , buttonParam = myButton: self.whichButton(k, m, buttonParam)
+   
+                myButton.pack()
                 table[key]= myButton
                 theWindow.update()
                 
 
 if __name__ == "__main__":
     theWindow = Tk()
-
     # Setting the title of the window
     theWindow.title("Connect 4")
-
     # Setting the geometry i.e Dimensions
     theWindow.geometry('800x800')
-
     theWindow['bg'] = "blue"
 
     # Calling our App
     menu = Menu()
-
-    yellowCoin = 0
-    redCoin = 1
-    turn = yellowCoin
-
-    action = Button(theWindow, text="clicked")
-
     # Calling our App
-    app = App(theWindow, turn)
+    app = App(theWindow)
 
 
     # to run infinitely
